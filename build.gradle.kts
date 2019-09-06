afa1950d22114596b0eb31d11b344fd8b84ce44a
KazaKago <weathercock.cage@gmail.com>
2019-09-06 23:27:40 +0900

    update kotlin 1.3.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath(kotlin("gradle-plugin", "1.3.21"))
         classpath("com.android.tools.build:gradle:3.5.0")
+        classpath(kotlin("gradle-plugin", "1.3.50"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
